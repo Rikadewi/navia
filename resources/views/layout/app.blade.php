087feec60510d097816d9165103133e85120cf5e
Rika Dewi <rika_4444@yahoo.com>
2019-10-15 01:08:06 +0700

    Add contact section

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -33,21 +33,88 @@
         <div class='navbar-link'>
             <a>About</a>
             <a>Products</a>
-            <a>Pricing</a>
             <a>Our Partners</a>
             <a>How To</a>
         </div>
         <div class="navbar-right">
-            <span class="round-button fill-button background-blue search-input">
+            <span class="round-button fill-button-thin background-blue search-input">
                 <input 
                     type='text' 
                     placeholder='Find Inspiration'
                 >
                 <a><i class='fa fa-search'></i></a>
             </span>
-            <a><i class="far fa-envelope"></i></a>
         </div>
     </div>
     @yield('body')
+    <footer class='section background-pastel'>
+        <div>
+            <div class='footer-title'>
+                <a><img class='footer-logo' src='../img/logo/logo-horizontal.png' alt='navia-logo'></a>
+            </div>
+            <h4>
+                Get Indonesian’s wooden furniture with the best craftsmen at affordable prices
+            </h4>
+            <br>
+            <a href="mailto:hello@naviafurniture.com" target="_blank">
+                <h4><i class="fa fa-envelope"></i> hello@naviafurniture.com</h4>
+            </a>
+            <a href="tel:+6282211511517" target="_blank">
+                <h4><i class="fa fa-phone"></i>+6289506217771</h4>
+            </a>
+        </div>
+        <div class='footer-link'>
+            <div class='footer-title'>
+                <h4 class='dark-blue'><b>Helpful Links</b></h4>
+            </div>
+            <a><h4>Products</h4></a>
+            <a><h4>How To</h4></a>
+            <a><h4>Find Inspiration</h4></a>
+            <br>
+            <div>
+                <a href="https://wa.me/+6282211511517" target="_blank">
+                    <img src='../img/icon/whatsapp.png' alt='whatsapp'>
+                </a>
+                <a href="https://wa.me/+6282211511517" target="_blank">
+                    <img class='middle-socmed' src='../img/icon/facebook.png' alt='facebook'>
+                </a>
+                <a href="https://wa.me/+6282211511517" target="_blank">
+                    <img src='../img/icon/linkedin.png' alt='linkedin'>
+                </a>
+            </div>
+        </div>
+        <div class='footer-contact'> 
+            <div class='footer-title'>
+                <h4 class='dark-blue'><b>Contact Us</b></h4>
+            </div>       
+            <span class='box-input'>
+                <i class='fa fa-user'></i>
+                <input 
+                    type='text' 
+                    placeholder='Enter your name'
+                >
+            </span>
+            <span class='box-input'>
+                <i class='fa fa-envelope'></i>
+                <input 
+                    type='text' 
+                    placeholder='Enter your email'
+                >
+            </span>
+            <span class='box-input'>
+                <i class='fa fa-edit'></i>
+                <input 
+                    type='text' 
+                    placeholder='Leave a message'
+                >
+            </span>
+            <br><br>
+            <div>
+                <span class='round-button'>
+                    Submit
+                </span>
+            </div>
+        </div>
+    </footer>
 </body>
 </html>

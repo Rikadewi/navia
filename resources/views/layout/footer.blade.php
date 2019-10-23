d581f0f9a14260302062d71491f03c1041d5944c
Rika Dewi <rika_4444@yahoo.com>
2019-10-23 15:43:00 +0700

    Responsive fotoer

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -1,69 +1,78 @@
-<footer class='section background-pastel'>
-    <div>
-        <div class='footer-title'>
-            <a><img class='footer-logo' src='../img/logo/logo-horizontal.png' alt='navia-logo'></a>
+<footer class='background-pastel'>
+    <div class=footer-wrapper>
+
+        <div class='footer-left'>
+            <div class='footer-title'>
+                <a href='/'><img class='footer-logo' src='../img/logo/logo-horizontal.png' alt='navia-logo'></a>
+            </div>
+            <div>
+                <h4>Reach Us On</h4>
+                <a href="mailto:hello@naviafurniture.com" target="_blank">
+                    <h4><i class="fa fa-envelope"></i> hello@naviafurniture.com</h4>
+                </a>
+                <a href="tel:+6282211511517" target="_blank">
+                    <h4><i class="fab fa-whatsapp"></i> +6289506217771</h4>
+                </a>
+            </div>
+            <div>
+                <h4>We Accept</h4>
+                <div class='footer-accept'>
+                    <img src='../img/logo/cc-mastercard-brands.png' alt='cc-mastercard-brands'>
+                    <img src='../img/logo/cc-visa-brands.png' alt='cc-visa-brands'>
+                    <img src='../img/logo/cc-paypal-brands.png' alt='cc-paypal-brands'>
+                    <img src='../img/logo/western-union.png' alt='western-union'>
+                </div>
+            </div>
+            <div class='footer-certified'>
+                <h4>Certified By</h4>
+                <a><img src='../img/logo/legal-wood.png' alt='indonesian-legal-wood'></a>
+            </div>
+    
         </div>
-        <h4>
-            Reach us on
-        </h4>
-        <br>
-        <a href="mailto:hello@naviafurniture.com" target="_blank">
-            <h4><i class="fa fa-envelope"></i> hello@naviafurniture.com</h4>
-        </a>
-        <a href="tel:+6282211511517" target="_blank">
-            <h4><i class="fa fa-phone"></i>+6289506217771</h4>
-        </a>
-    </div>
-    <div class='footer-link'>
-        <div class='footer-title'>
-            <h4 class='dark-blue'><b>Helpful Links</b></h4>
+        <div class='footer-link'>
+            <div class='footer-title'>
+                <h4 class='dark-blue'><b>Helpful Links</b></h4>
+            </div>
+            <a><h4>Home</h4></a>
+            <a><h4>About</h4></a>
+            <a><h4>Products</h4></a>
+            <a><h4>How To</h4></a>
+            <a><h4>Find Inspiration</h4></a>
+            <a><h4>Contact</h4></a>
         </div>
-        <a><h4>Products</h4></a>
-        <a><h4>How To</h4></a>
-        <a><h4>Find Inspiration</h4></a>
-        <br>
-        <div>
-            <a href="https://wa.me/+6282211511517" target="_blank">
-                <img src='../img/icon/whatsapp.png' alt='whatsapp'>
-            </a>
-            <a href="https://wa.me/+6282211511517" target="_blank">
-                <img class='middle-socmed' src='../img/icon/facebook.png' alt='facebook'>
-            </a>
-            <a href="https://wa.me/+6282211511517" target="_blank">
-                <img src='../img/icon/linkedin.png' alt='linkedin'>
-            </a>
-        </div>
-    </div>
-    <div class='footer-contact'> 
-        <div class='footer-title'>
-            <h4 class='dark-blue'><b>Contact Us</b></h4>
-        </div>       
-        <span class='box-input'>
-            <i class='fa fa-user'></i>
-            <input 
-                type='text' 
-                placeholder='Enter your name'
-            >
-        </span>
-        <span class='box-input'>
-            <i class='fa fa-envelope'></i>
-            <input 
-                type='text' 
-                placeholder='Enter your email'
-            >
-        </span>
-        <span class='box-input'>
-            <i class='fa fa-edit'></i>
-            <input 
-                type='text' 
-                placeholder='Leave a message'
-            >
-        </span>
-        <br><br>
-        <div>
-            <span class='round-button'>
-                Submit
+        <div class='footer-contact'> 
+            <div class='footer-title'>
+                <h4 class='dark-blue'><b>Contact Us</b></h4>
+            </div>       
+            <span class='box-input'>
+                <i class='fa fa-user'></i>
+                <input 
+                    type='text' 
+                    placeholder='Enter your name'
+                >
             </span>
+            <span class='box-input'>
+                <i class='fa fa-envelope'></i>
+                <input 
+                    type='text' 
+                    placeholder='Enter your email'
+                >
+            </span>
+            <span class='box-input'>
+                <i class='fa fa-edit'></i>
+                <input 
+                    type='text' 
+                    placeholder='Leave a message'
+                >
+            </span>
+            <div class='footer-submit'>
+                <span class='round-button'>
+                    Submit
+                </span>
+            </div>
         </div>
     </div>
+    <h4 class='copy'>
+        &copy; Navia, 2019.
+    </h4>
 </footer>
--- resources/views/layout/navbar.blade.php
+++ resources/views/layout/navbar.blade.php
@@ -3,10 +3,11 @@
         <a class='control-mobile' data-toggle = "collapse" data-target = "#collapsed"><img src='../img/icon/menu.svg' alt='menubar'></a>
         <img src='../img/logo/logo-horizontal.png' alt='navia-logo'>
         <div class='navbar-link'>
+            <a>Home</a>
             <a>About</a>
             <a>Products</a>
-            <a>Our Partners</a>
-            <a class='no-border-bottom'>How To</a>
+            <a>How To</a>
+            <a>Contact</a>
         </div>
         <div class="right-navbar">
             <span id='search-button' class="round-button fill-button background-blue search-input">
@@ -21,9 +22,11 @@
         </div>
     </div>
     <div id='collapsed' class='background-pastel'>
+        <a>Home</a>
         <a>About</a>
         <a>Products</a>
-        <a>Our Partners</a>
+        <a>Partners</a>
         <a>How To</a>
+        <a class='no-border-bottom'>Contact</a>
     </div>
 </div>
